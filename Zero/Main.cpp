a298d065367c0dbdd3bbce0282677a628caf8a84
anon <anon>
2026-07-18 11:55:31 +0000

    Retro builds: Linux, Linux 64-bit, Win32.

--- Zero/Main.cpp
+++ Zero/Main.cpp
@@ -1,13 +1,37 @@
 
 #include <cstdio>
+#include <SDL2/SDL.h>
 #include "Main.hpp"
 
-int main() {
-  const int res = SDL_Init(SDL_INIT_EVERYTHING);
+int main(int argc, char** argv) {
+  const int res = SDL_Init(0);
   if (res == 0) {
     printf("SDL Init Successful!\n");
   }
+  SDL_Window* window = SDL_CreateWindow(
+    "Zero",
+    800, 600,
+    0
+  );
+  if (window == null) {
+    printf("SDL Window Creation Failed!\n");
+    printf("%s\n", SDL_GetError());
+    goto exit;
+  }
   printf("AB_PROFILE: %s\n", stringify(AB_PROFILE));
   printf("Hello my dear friends!\n");
+  printf("Video driver: %s\n", SDL_GetCurrentVideoDriver());
+  SDL_Event event;
+  while (true) {
+    while (SDL_PollEvent(&event)) {
+      if (event.type == SDL_QUIT) {
+        goto exit;
+      }
+    }
+  }
+  exit:
+  SDL_DestroyWindow(window);
   SDL_Quit();
+  printf("Goodbye\n");
+  return 0;
 }
--- retro-linux-64.cmake
+++ retro-linux-64.cmake
@@ -1,42 +1,42 @@
 
 # Target System
 set(CMAKE_SYSTEM_NAME Linux)
-set(CMAKE_SYSTEM_PROCESSOR x86)
+set(CMAKE_SYSTEM_PROCESSOR AMD64)
 
 # Compiler
-set(CMAKE_C_COMPILER i686-linux-gnu-gcc)
-set(CMAKE_CXX_COMPILER i686-linux-gnu-g++)
+set(CMAKE_C_COMPILER gcc)
+set(CMAKE_CXX_COMPILER g++)
 
 # SysRoot
 get_filename_component(AB_TOOLCHAIN_DIR "${CMAKE_CURRENT_LIST_DIR}" ABSOLUTE)
 set(CMAKE_SYSROOT "${AB_TOOLCHAIN_DIR}/BuildEnv/CentOS")
 string(APPEND CMAKE_C_FLAGS_INIT
   " --sysroot=${CMAKE_SYSROOT}"
-  " -B${CMAKE_SYSROOT}/usr/lib/gcc/i686-redhat-linux/4.8.5"
-  " -B${CMAKE_SYSROOT}/usr/lib"
+  " -B${CMAKE_SYSROOT}/usr/lib/gcc/x86_64-redhat-linux/4.8.5"
+  " -B${CMAKE_SYSROOT}/usr/lib64"
   " -isystem ${CMAKE_SYSROOT}/usr/include"
-  " -m32"
+  " -m64"
 )
 string(APPEND CMAKE_CXX_FLAGS_INIT
   " --sysroot=${CMAKE_SYSROOT}"
-  " -B${CMAKE_SYSROOT}/usr/lib/gcc/i686-redhat-linux/4.8.5"
-  " -B${CMAKE_SYSROOT}/usr/lib"
+  " -B${CMAKE_SYSROOT}/usr/lib/gcc/x86_64-redhat-linux/4.8.5"
+  " -B${CMAKE_SYSROOT}/usr/lib64"
   " -isystem ${CMAKE_SYSROOT}/usr/include"
   " -isystem ${CMAKE_SYSROOT}/usr/include/c++/4.8.5"
-  " -isystem ${CMAKE_SYSROOT}/usr/include/c++/4.8.5/i686-redhat-linux"
-  " -m32"
+  " -isystem ${CMAKE_SYSROOT}/usr/include/c++/4.8.5/x86_64-redhat-linux"
+  " -m64"
 )
 string(APPEND CMAKE_EXE_LINKER_FLAGS_INIT
-  " -B${CMAKE_SYSROOT}/usr/lib/gcc/i686-redhat-linux/4.8.5"
-  " -L${CMAKE_SYSROOT}/usr/lib"
+  " -B${CMAKE_SYSROOT}/usr/lib/gcc/x86_64-redhat-linux/4.8.5"
+  " -L${CMAKE_SYSROOT}/usr/lib64"
   " -L${CMAKE_SYSROOT}/lib"
-  " -m32"
+  " -m64"
 )
 string(APPEND CMAKE_SHARED_LINKER_FLAGS_INIT
   " ${CMAKE_EXE_LINKER_FLAGS_INIT}"
-  " -m32"
+  " -m64"
 )
-string(APPEND CMAKE_MODULE_LINKER_FLAGS_INIT " -m32")
+string(APPEND CMAKE_MODULE_LINKER_FLAGS_INIT " -m64")
 
 # Search Behavior
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
