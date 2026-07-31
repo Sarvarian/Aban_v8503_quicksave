aecaf04969313a2f1c3dca3fb959987f9fe690c3
anon <mail@example.com>
2026-07-31 18:45:02 +0330

    New Main

--- Src/Main.cpp
+++ Src/Main.cpp
@@ -1,5 +1,3 @@
-
-#include <cstdio>
 #include "Main.hpp"
 
 #if IS_USING_SDL_3
@@ -7,55 +5,67 @@
   #include <SDL3/SDL_main.h>
 #elif IS_USING_SDL_2
   #include <SDL2/SDL.h>
-#elif IS_USING_SDL_1
-  #include <SDL/SDL.h>
-#else
-  #error "Failed to detect SDL version."
+  /* ReSharper disable once CppUnusedIncludeDirective */
+  #include <SDL2/SDL_main.h>
 #endif
 
-#include "Sdl.hpp"
+#include "Engine.hpp"
 
-int main(int argc, char** argv) {
-  int res = 0;
-  SdlWindow window = SdlWindow();
-  res = Sdl().video().init();
-  if (res == Sdl::INIT_SUCCEED) {
-    printf("SDL Init Successful!\n");
-  } else {
-    printf("SDL Init Failed!\nSDL: %s\n", SDL_GetError());
-    goto exit;
+
+struct Timing {
+private:
+  const u64 frequency_;
+  u64 post_;
+  u32 delay_;
+  Timing()
+  : frequency_(clockFrequencyU64() / MSPS)
+  , post_(clockU64())
+  , delay_(0)
+  {}
+public:
+  void frameEnd(const u32 target_delta_ms) {
+    const u64 now = clockU64();
+    const u64 past = post_ + delay_;
+    const u64 delta = (now - past) / frequency_;
+    delay_
+    = target_delta_ms > delta
+    ? target_delta_ms - delta
+    : 0
+    ;
+    SDL_Delay(delay_);
+    post_ = clockU64();
   }
-  if (window.create() == SdlWindow::CREATION_FAILED) {
-    printf("SDL Window Creation Failed!\n");
-    printf("%s\n", SDL_GetError());
-    goto exit;
+  static Timing def() {
+    return Timing();
   }
-  printf("Hello my dear friends!\n");
-#if !IS_USING_SDL_1
-  {
-    const int video_count = SdlWindow::getVideoDriverCount();
-    if (video_count < 1) {
-      goto exit;
-    }
-    printf("Found %d video drivers.\n", video_count);
-    for (int i = 0; i < video_count; i++) {
-      const char* name = SdlWindow::getVideoDriverName(i);
-      printf("Video driver %d: %s\n", i, name);
-    }
-  }
-#endif
-  printf("Current video driver: %s\n", SdlWindow::getCurrentVideoDriverName());
-  SDL_Event event;
-  while (true) {
-    while (SDL_PollEvent(&event)) {
-      if (event.type == SDL_EVENT_QUIT) {
-        goto exit;
-      }
-    }
-  }
-  exit:
-  window.destroy();
-  Sdl::quit();
-  printf("Goodbye\n");
-  return 0;
+};
+
+static int shutdown(Engine& engine) {
+  const int exit_code = engine.exit_code;
+  engine.shutEngine();
+  engine.shutSdl();
+  return exit_code;
 }
+
+#define CHECK(ROUTINE_CALL) \
+  if ((ROUTINE_CALL) != E_SYS_CONTINUE) { return shutdown(engine); }
+
+/* ReSharper disable once CppParameterMayBeConst */
+int main(int argc, char** argv) {
+  Engine engine = Engine::def();
+  CHECK(engine.preSdlInit(argc, argv))
+  CHECK(engine.initSdl(argc, argv));
+  CHECK(engine.initEngine(argc, argv));
+  Timing timing = Timing::def();
+  while (true) {
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+      CHECK(engine.eventSdl(&event));
+    }
+    CHECK(engine.stepEngine());
+    timing.frameEnd(engine.target_delta_ms);
+  }
+  /* ReSharper disable once CppDFAUnreachableCode */
+  return shutdown(engine);
+}
+
