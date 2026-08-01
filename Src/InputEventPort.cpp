c481242a1ef94839c4e11d4a5ce122b032cbc526
anon <mail@example.com>
2026-08-01 19:30:22 +0330

    update

--- Src/InputEventPort.cpp
+++ Src/InputEventPort.cpp
@@ -6,7 +6,71 @@ EventReceiver EventReceiver::def() {
   return EventReceiver();
 }
 
+#if IS_USING_SDL_1
 ESysStatus EventReceiver::active(const SDL_ActiveEvent &event) {
+  return E_SYS_CONTINUE;
+}
+
+ESysStatus EventReceiver::resize(const SDL_ResizeEvent &event) {
+  return E_SYS_CONTINUE;
+}
+
+ESysStatus EventReceiver::expose(const SDL_ExposeEvent &event) {
+  return E_SYS_CONTINUE;
+}
+#endif
+
+ESysStatus EventReceiver::quit(const SDL_QuitEvent &event) {
+  return E_SYS_QUIT;
+}
+
+#if IS_USING_SDL_1 || IS_USING_SDL_2
+ESysStatus EventReceiver::sysWM(const SDL_SysWMEvent &event) {
+  return E_SYS_CONTINUE;
+}
+#endif
+
+ESysStatus EventReceiver::keyDown(const SDL_KeyboardEvent &event) {
+  return E_SYS_CONTINUE;
+}
+
+ESysStatus EventReceiver::keyUp(const SDL_KeyboardEvent &event) {
+  return E_SYS_CONTINUE;
+}
+
+ESysStatus EventReceiver::motion(const SDL_MouseMotionEvent &event) {
+  return E_SYS_CONTINUE;
+}
+
+ESysStatus EventReceiver::buttonDown(const SDL_MouseButtonEvent &event) {
+  return E_SYS_CONTINUE;
+}
+
+ESysStatus EventReceiver::buttonUp(const SDL_MouseButtonEvent &event) {
+  return E_SYS_CONTINUE;
+}
+
+ESysStatus EventReceiver::axis(const SDL_JoyAxisEvent &event) {
+  return E_SYS_CONTINUE;
+}
+
+ESysStatus EventReceiver::ball(const SDL_JoyBallEvent &event) {
+  return E_SYS_CONTINUE;
+}
+
+ESysStatus EventReceiver::hat(const SDL_JoyHatEvent &event) {
+  return E_SYS_CONTINUE;
+}
+
+ESysStatus EventReceiver::joyDown(const SDL_JoyButtonEvent &event) {
+  return E_SYS_CONTINUE;
+}
+
+ESysStatus EventReceiver::joyUp(const SDL_JoyButtonEvent &event) {
+  return E_SYS_CONTINUE;
+}
+
+ESysStatus EventReceiver::user(const SDL_UserEvent &event) {
   return E_SYS_CONTINUE;
 }
 
@@ -15,22 +79,28 @@ ESysStatus EventReceiver::active(const SDL_ActiveEvent &event) {
 ESysStatus EventReceiver::receiveSdlInputEvent(const SDL_Event &event) {
   switch (event.type) {
     case SDL_ACTIVEEVENT: return active(event.active);
-    case SDL_KEYDOWN: break;
-    case SDL_KEYUP: break;
-    case SDL_MOUSEMOTION: break;
-    case SDL_MOUSEBUTTONDOWN: break;
-    case SDL_MOUSEBUTTONUP: break;
-    case SDL_JOYAXISMOTION: break;
-    case SDL_JOYBALLMOTION: break;
-    case SDL_JOYHATMOTION: break;
-    case SDL_JOYBUTTONDOWN: break;
-    case SDL_JOYBUTTONUP: break;
-    case SDL_QUIT: break;
-    case SDL_SYSWMEVENT: break;
-    case SDL_VIDEORESIZE: break;
-    case SDL_VIDEOEXPOSE: break;
-    case SDL_USEREVENT: break;
-    default: break;
+    case SDL_KEYDOWN: return keyDown(event.key);
+    case SDL_KEYUP: return keyUp(event.key);
+    case SDL_MOUSEMOTION: return motion(event.motion);
+    case SDL_MOUSEBUTTONDOWN: return buttonDown(event.button);
+    case SDL_MOUSEBUTTONUP: return buttonUp(event.button);
+    case SDL_JOYAXISMOTION: return axis(event.jaxis);
+    case SDL_JOYBALLMOTION: return ball(event.jball);
+    case SDL_JOYHATMOTION: return hat(event.jhat);
+    case SDL_JOYBUTTONDOWN: return joyDown(event.jbutton);
+    case SDL_JOYBUTTONUP: return joyUp(event.jbutton);
+    case SDL_QUIT: return quit(event.quit);
+    case SDL_SYSWMEVENT: return sysWM(event.syswm);
+    case SDL_VIDEORESIZE: return resize(event.resize);
+    case SDL_VIDEOEXPOSE: return expose(event.expose);
+    case SDL_USEREVENT: return user(event.user);
+    default: {
+      /* I don't think `default` should be reachable. */
+      /* I put a debugBreak so if it ever happened,   */
+      /*     I get a change to investigate it.        */
+      debugBreak;
+      return E_SYS_CONTINUE;
+    };
   }
   return E_SYS_CONTINUE;
 }
--- Src/InputEventPort.hpp
+++ Src/InputEventPort.hpp
@@ -7,10 +7,8 @@
 
 #if IS_USING_SDL_3
   #include <SDL3/SDL_events.h>
-  struct SDL_ActiveEvent;
 #elif IS_USING_SDL_2
   #include <SDL2/SDL_events.h>
-  struct SDL_ActiveEvent;
 #elif IS_USING_SDL_1
   #include <SDL/SDL_events.h>
 #endif
@@ -23,8 +21,37 @@ public:
 private:
   EventReceiver();
 
+#if IS_USING_SDL_1
   /** SDL1 Active Event */
   ESysStatus active(const SDL_ActiveEvent& event);
+  /** SDL1 Resize Event */
+  ESysStatus resize(const SDL_ResizeEvent& event);
+  /** SDL1 Expose Event */
+  ESysStatus expose(const SDL_ExposeEvent& event);
+#endif
+
+  /* SDL1, SDL2, SDL3 Common Events Begin */
+  ESysStatus quit(const SDL_QuitEvent& event);
+  /* SDL1, SDL2, SDL3 Common Events End */
+
+#if IS_USING_SDL_1 || IS_USING_SDL_2
+  /** SDL1, SDL2 SysWM Event */
+  ESysStatus sysWM(const SDL_SysWMEvent& event);
+#endif
+
+  /* SDL1, SDL2, SDL3 Common Events Begin */
+  ESysStatus keyDown(const SDL_KeyboardEvent& event); /**< Keyboard Key Down */
+  ESysStatus keyUp(const SDL_KeyboardEvent& event); /**< Keyboard Key Up */
+  ESysStatus motion(const SDL_MouseMotionEvent& event); /**< Mouse Motion */
+  ESysStatus buttonDown(const SDL_MouseButtonEvent& event); /**< Mouse Button Down */
+  ESysStatus buttonUp(const SDL_MouseButtonEvent& event); /**< Mouse Button Up */
+  ESysStatus axis(const SDL_JoyAxisEvent& event); /**< Joystick Axis Motion */
+  ESysStatus ball(const SDL_JoyBallEvent& event); /**< Joystick Ball Motion */
+  ESysStatus hat(const SDL_JoyHatEvent& event); /**< Joystick Hat Motion */
+  ESysStatus joyDown(const SDL_JoyButtonEvent& event); /**< Joystick Button Down */
+  ESysStatus joyUp(const SDL_JoyButtonEvent& event); /**< Joystick Button Up */
+  ESysStatus user(const SDL_UserEvent& event); /**< "User" Custom Event */
+  /* SDL1, SDL2, SDL3 Common Events End */
 
 };
 
