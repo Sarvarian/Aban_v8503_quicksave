315fce3900f90f7499757f3baef51692828108ac
anon <mail@example.com>
2026-08-02 00:16:02 +0330

    update

--- Src/InputEventPort.cpp
+++ Src/InputEventPort.cpp
@@ -159,7 +159,7 @@ ESysStatus EventReceiver::off(const SDL_DisplayEvent& event) {
   return E_SYS_CONTINUE;
 }
 
-ESysStatus EventReceiver::reposition(const SDL_DisplayEvent& event) {
+ESysStatus EventReceiver::relocation(const SDL_DisplayEvent& event) {
   return E_SYS_CONTINUE;
 }
 #endif
@@ -214,7 +214,7 @@ ESysStatus EventReceiver::receiveSdlInputEvent(const SDL_Event& event) {
           }
         case SDL_DISPLAYEVENT_CONNECTED: return on(event.display);
         case SDL_DISPLAYEVENT_DISCONNECTED: return off(event.display);
-        case SDL_DISPLAYEVENT_MOVED: reposition(event.display);
+        case SDL_DISPLAYEVENT_MOVED: return relocation(event.display);
         default: return unrecognizedDisplayEvent(event.display);
       }
     default: return unrecognized(event);
--- Src/InputEventPort.hpp
+++ Src/InputEventPort.hpp
@@ -81,7 +81,7 @@ private:
   ESysStatus portraitFlipped(const SDL_DisplayEvent& event); /**< Display Orientation Changed, Portrait Flipped */
   ESysStatus on(const SDL_DisplayEvent& event); /**< System Display Added */
   ESysStatus off(const SDL_DisplayEvent& event); /**< System Display Removed */
-  ESysStatus reposition(const SDL_DisplayEvent& event); /**< System Display Position Changed */
+  ESysStatus relocation(const SDL_DisplayEvent& event); /**< System Display Position Changed */
   /* SDL2 Only and SDL3 Only Events End */
 #endif
 
