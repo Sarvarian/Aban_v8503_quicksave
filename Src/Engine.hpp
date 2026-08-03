16646908ad1a398c4853302bd136c6a1d3da08e9
anon <mail@example.com>
2026-08-03 15:15:10 +0330

    update

--- Src/Engine.hpp
+++ Src/Engine.hpp
@@ -7,20 +7,6 @@
 #include "Memory.hpp"
 #include "Sdl.hpp"
 
-struct Step {
-public:
-  u8 just_continue : 1;
-  u8 calculate_delta_time : 1;
-  u8 window_check : 1;
-  u8 window_undef : 1;
-  u8 window_def : 1;
-  u8 _pad03_ : 1;
-  u8 _pad02_ : 1;
-  u8 _pad01_ : 1;
-  u8 _pad00_[mmBufferSize(0) - 1];
-};
-staticAssert(sizeof(Step) == sizeof(Buffer0), Step_FIT_INTO_ONE_Buffer0)
-
 class Engine {
 public:
   static Engine def();
@@ -47,6 +33,20 @@ public:
 
 private:
   Engine();
+
+  struct Step {
+  public:
+    u8 just_continue : 1;
+    u8 calculate_delta_time : 1;
+    u8 window_check : 1;
+    u8 window_undef : 1;
+    u8 window_def : 1;
+    u8 _pad03_ : 1;
+    u8 _pad02_ : 1;
+    u8 _pad01_ : 1;
+    u8 _pad00_[mmBufferSize(0) - 1];
+  };
+  staticAssert(sizeof(Step) == sizeof(Buffer0), Step_FIT_INTO_ONE_Buffer0)
 
   SdlWindow window_;
   struct DebugData* db_;
