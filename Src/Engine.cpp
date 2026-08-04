44dcac7b31cf0660c81a2edacbdcdf790797ade0
anon <mail@example.com>
2026-08-04 22:38:58 +0330

    update

--- Src/Engine.cpp
+++ Src/Engine.cpp
@@ -481,10 +481,10 @@ ESysStatus Engine::on_sys_wm(const SDL_SysWMEvent& syswm) {
 */
 
 ESysStatus Engine::on_keyboard_key_down(const SDL_KeyboardEvent& key) {
-#if IS_USING_SDL_3
-  switch (key.key) {
-#elif IS_USING_SDL_1 || IS_USING_SDL_2
+#if IS_USING_SDL_1 || IS_USING_SDL_2
   switch (key.keysym.sym) {
+#elif IS_USING_SDL_3
+  switch (key.key) {
 #endif
   case SDLK_0: target_delta_ms = 0; break;
   case SDLK_1: target_delta_ms = (MSPS / 30); break;
