cec5cbbfe02819e38fd253adb5c7d1ec86f09c2a
anon <mail@example.com>
2026-08-09 17:39:28 +0330

    Update

---
+++ Src/Asr.hpp
@@ -0,0 +1,10 @@
+#ifndef AB_ASR_HPP
+#define AB_ASR_HPP
+
+/* Aban Strong Renderer */
+
+class Asr {
+
+};
+
+#endif /* AB_ASR_HPP */
---
+++ Src/Ass.hpp
@@ -0,0 +1,10 @@
+#ifndef AB_ASS_HPP
+#define AB_ASS_HPP
+
+/* Aban Simulation System */
+
+class Ass {
+
+};
+
+#endif /* AB_ASS_HPP */
