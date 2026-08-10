762d3be7fd4b314b4bbd6f18140b085c2a0e695f
anon <anon>
2026-08-10 16:13:37 +0330

    update

--- Src/Main.hpp
+++ Src/Main.hpp
@@ -1711,51 +1711,6 @@ public:
 
 };
 
-/** SDL Window Handle Manager */
-class SdlWindow {
-public:
-
-  static SdlWindow def();
-
-#if IS_USING_SDL_1
-  typedef SDL_Surface Handle;
-#elif IS_USING_SDL_2 || IS_USING_SDL_3
-  typedef SDL_Window Handle;
-#endif
-
-  /** Returns null in case of failure. Call SDL_GetError() for more information. */
-  Handle* create();
-  /** Always returns null.*/
-  Handle* destroy();
-
-  /** Wrapper around `SDL_GetNumVideoDrivers`
-   *  - SDL1: Will always return -1
-   */
-  static int getVideoDriverCount();
-
-  /** Wrapper around `SDL_GetVideoDriver`
-   *  - SDL1: Will always return null
-   */
-  static const char* getVideoDriverName(const int index);
-
-  /** Wrapper around `SDL_GetCurrentVideoDriver`
-   *  - SDL1: Will return result of `SDL_VideoDriverName`
-   *  - SDL1: Returns null if video has not been initialized with SDL_Init
-   */
-  static const char* getCurrentVideoDriverName();
-
-  /** \returns true on success or false on failure; call SDL_GetError() for more information. */
-  bool setTitle(const char* title);
-
-private:
-  SdlWindow();
-
-  /** On SDL1 this will be `SDL_Surface`
-   *  while in SDl2 and SDL3 this will be `SDL_Window` */
-  Handle* handle_;
-};
-staticAssert(sizeof(SdlWindow) == sizeof(void*), SdlWindow_IS_JUST_A_CONTAINER_FOR_ITS_HANDLE)
-
 #if AB_VULKAN
 #include <volk.h>
 class VulkanAppInfo : private VkApplicationInfo {
--- Src/Video.cpp
+++ Src/Video.cpp
@@ -1,5 +1,83 @@
 #include "Video.hpp"
 
+SdlWindow::SdlWindow() {
+  handle_ = null;
+}
+
+SdlWindow SdlWindow::def() {
+  return SdlWindow();
+}
+
+SdlWindow::Handle* SdlWindow::create() {
+#if IS_USING_SDL_1
+  handle_ = SDL_SetVideoMode(640, 480, 0, 0);
+  SDL_WM_SetCaption("Aban", null);
+#elif IS_USING_SDL_2
+  handle_ = SDL_CreateWindow(
+    "Aban",
+    SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+    800, 600,
+    0
+  );
+#elif IS_USING_SDL_3
+  handle_ = SDL_CreateWindow("Aban", 800, 600, 0);
+#endif
+  return handle_;
+}
+
+// ReSharper disable once CppDFAConstantFunctionResult
+SdlWindow::Handle* SdlWindow::destroy() {
+#if IS_USING_SDL_1
+  handle_ = null;
+#elif  IS_USING_SDL_2 || IS_USING_SDL_3
+  if (handle_ != null) { SDL_DestroyWindow(handle_); }
+  handle_ = null;
+#endif
+  return handle_;
+}
+
+int SdlWindow::getVideoDriverCount() {
+#if IS_USING_SDL_1
+  return -1;
+#elif IS_USING_SDL_2 || IS_USING_SDL_3
+  return SDL_GetNumVideoDrivers();
+#endif
+}
+
+const char* SdlWindow::getVideoDriverName(const int index) {
+#if IS_USING_SDL_1
+  return null;
+#elif IS_USING_SDL_2 || IS_USING_SDL_3
+  return SDL_GetVideoDriver(index);
+#endif
+}
+
+#if IS_USING_SDL_1
+static char current_video_driver_name_[255];
+#endif
+
+const char* SdlWindow::getCurrentVideoDriverName() {
+#if IS_USING_SDL_1
+  memset(current_video_driver_name_, 0, sizeof(current_video_driver_name_));
+  SDL_VideoDriverName(current_video_driver_name_, sizeof(current_video_driver_name_));
+  return current_video_driver_name_;
+#elif IS_USING_SDL_2 || IS_USING_SDL_3
+  return SDL_GetCurrentVideoDriver();
+#endif
+}
+
+bool SdlWindow::setTitle(const char* title) {
+#if IS_USING_SDL_1
+  SDL_WM_SetCaption(title, null);
+  return true;
+#elif  IS_USING_SDL_2
+  SDL_SetWindowTitle(handle_, title);
+  return true;
+#elif  IS_USING_SDL_3
+  return SDL_SetWindowTitle(handle_, title);
+#endif
+}
+
 Video::Video() {
 }
 
