6d3457e833e40470278a0bfe6d9c9e82398a0960
anon <anon>
2026-08-16 07:31:04 +0330

    update

--- Src/Ace.hpp
+++ Src/Ace.hpp
@@ -6,6 +6,14 @@
 /** Aban Computation Engine (aka math engine) */
 class Ace {
 
+  /*
+   *  Output: SDL_Window with OpenGL
+   *  Input Port: Velocity 2D
+   *  Computation: 
+   *
+   */
+
+
 };
 
 #endif /* AB_ACE_HPP */
